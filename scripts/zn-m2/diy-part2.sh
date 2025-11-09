2e6a1793294b0a35ea539ae09c675bee2b13e321
crazy66688 <crazyhalf@126.com>
2025-11-09 22:09:30 +0800

    Update diy-part2.sh

--- scripts/zn-m2/diy-part2.sh
+++ scripts/zn-m2/diy-part2.sh
@@ -12,3 +12,7 @@
 #优先安装 passwall 源
 ./scripts/feeds install -a -f -p passwall_packages
 ./scripts/feeds install -a -f -p passwall_luci
+git clone --depth=1 https://github.com/nikkinikki-org/OpenWrt-nikki package/nikki
+git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
+git clone https://github.com/sirpdboy/luci-app-timecontrol package/luci-app-timecontrol
+git clone https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
